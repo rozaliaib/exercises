e22dc9f0478eeaf19b15e35166cb8895f05c83f4
Your Name <you@example.com>
2021-10-10 11:06:45 +0300

    second commit

---
+++ Ex_1.py
@@ -0,0 +1,7 @@
+class cat:
+    name = 'Матроскин'
+    color = 'black'
+
+my_cat = cat()
+
+print(my_cat.name, my_cat.color)
---
+++ Ex_2.py
@@ -0,0 +1,8 @@
+class Lion:
+
+    def roar(self):
+         print('Rrrrrrr!!!')
+
+simba = Lion()
+simba.roar()
+
